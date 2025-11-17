c4f26fcbae5084eab997104be765357a13abda98
ryan-achten_xero <ryan.achten@xero.com>
2025-11-17 16:48:33 +1300

    feat: update consumer to use order topic

--- KafkaConsumer/Program.cs
+++ KafkaConsumer/Program.cs
@@ -1,28 +1,33 @@
 using Confluent.Kafka;
+using Common;
 using static Common.Constants;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var kafkaConfig = builder.Configuration.GetRequiredSection(KafkaConfiguration.SectionName).Get<KafkaConfiguration>()
+    ?? throw new InvalidOperationException("Kafka configuration is missing");
 
 /// Allows for demonstration of how consumers are treated differently
 /// when they are part of the same group, versus when they have different group IDs
 /// - If consumers have the same group ID, messages should be allocated between them
 /// - If consumers have different group IDs, they should receive all messages in a broadcast-fashion
-Console.WriteLine("Enter consumer group ID:");
-var groupId = Console.ReadLine();
+var groupId = Environment.GetEnvironmentVariable("GROUP_ID");
 if (groupId == null || groupId == string.Empty)
 {
-    throw new ArgumentException("Group ID cannot be empty");
+    throw new ArgumentException("GROUP_ID cannot be empty");
 }
 
 var config = new ConsumerConfig()
 {
     GroupId = groupId,
-    BootstrapServers = Configuration.KafkaConnectUri,
+    BootstrapServers = kafkaConfig.BootstrapServers,
     AutoOffsetReset = AutoOffsetReset.Earliest,
 };
 
 using var consumer = new ConsumerBuilder<Null, string>(config).Build();
 using var tokenSource = new CancellationTokenSource();
 
-consumer.Subscribe(Topics.Weather);
+consumer.Subscribe(Topics.Orders);
 
 Console.WriteLine("Messages will appear below:");
 try
