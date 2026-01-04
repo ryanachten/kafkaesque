fd8d4b4fdc4d6943305dc3ec3d38edc65d0c456c
ryanachten <ryanachten@gmail.com>
2026-01-05 08:33:33 +1300

    refactor: separate order into domain model and DTO

--- OrderService/Models/Order.cs
+++ OrderService/Models/Order.cs
@@ -1,5 +1,33 @@
+using NanoidDotNet;
+using OrderService.Models.DTOs;
+
 namespace OrderService.Models;
 
-public record Order(IEnumerable<OrderItem> Items);
+public class Order
+{
+    public Order()
+    {
+        OrderShortCode = Nanoid.Generate(size: 10);
+    }
 
-public record OrderItem(string Name, int Count);
+    public Order(CreateOrderRequest dto)
+    {
+        OrderShortCode = Nanoid.Generate(size: 10);
+        CustomerId = dto.CustomerId;
+        Items = dto.Items.Select(item => new OrderItem(item.ProductId, item.Count));
+    }
+
+    public string OrderShortCode { get; private set; }
+    public Guid CustomerId { get; set; }
+    public IEnumerable<OrderItem> Items { get; set; } = [];
+    public OrderStatus Status { get; set; } = OrderStatus.PENDING;
+}
+
+public enum OrderStatus
+{
+    PENDING,
+    FULFILLED,
+    SHIPPED
+}
+
+public record OrderItem(Guid ProductId, int Count);
